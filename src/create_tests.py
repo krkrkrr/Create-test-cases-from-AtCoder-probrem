9fc1dc441e588e7c67609dda3174e96efc2e69b6
krkrkrr <67230121+krkrkrr@users.noreply.github.com>
2025-02-13 04:58:48 +0900

    [add] test cases for create_tests

---
+++ src/create_tests.py
@@ -0,0 +1,11 @@
+def create_tests(test_cases: set[tuple[str, str]]) -> str:
+    """
+    Create test cases for the function solve.
+    
+    Args:
+        test_cases (set[tuple[str, str]]): A set of test cases.
+    
+    Returns:
+        str: The test cases for the function solve.
+    """
+    raise NotImplementedError
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,5 +1,6 @@
 import pytest
 from get_test_cases import get_test_cases
+from create_tests import create_tests
 
 
 def test_get_test_cases():
@@ -13,6 +14,67 @@ def test_get_test_cases():
 
     # Act
     result = get_test_cases(actual)
+
+    # Assert
+    assert result == expect
+
+
+def test_create_tests():
+    # Assert
+    actual = set([
+        ('3 15 5\n', 'Yes\n'),
+        ('5 3 2\n', 'No\n'),
+        ('3 3 9\n', 'Yes\n'),
+    ])
+    expect = """
+from a import solve
+
+import io
+
+
+def test_a_solve_1(capsys, monkeypatch):
+    actual = \"\"\"\\
+3 15 5
+
+\"\"\"
+    monkeypatch.setattr('sys.stdin', io.StringIO(actual))
+    solve()
+    expected = \"\"\"\\
+Yes
+
+\"\"\"
+    assert capsys.readouterr().out == expected
+
+
+def test_a_solve_2(capsys, monkeypatch):
+    actual = \"\"\"\\
+5 3 2
+
+\"\"\"
+    monkeypatch.setattr('sys.stdin', io.StringIO(actual))
+    solve()
+    expected = \"\"\"\\
+No
+
+\"\"\"
+    assert capsys.readouterr().out == expected
+
+
+def test_a_solve_3(capsys, monkeypatch):
+    actual = \"\"\"\\
+3 3 9
+
+\"\"\"
+    monkeypatch.setattr('sys.stdin', io.StringIO(actual))
+    solve()
+    expected = \"\"\"\\
+Yes
+
+\"\"\"
+    assert capsys.readouterr().out == expected
+"""
+    # Act
+    result = create_tests(actual)
 
     # Assert
     assert result == expect
